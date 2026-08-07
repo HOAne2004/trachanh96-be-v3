b2f678c3fc3b0b40fa0e0d760f8420c92cd37c70
HOAn04 <lehuyhoan04@gmail.com>
2026-08-07 22:20:23 +0700

    feat(orders): update audit columns to order.

--- src/Modules/Orders/Orders.Infrastructure/Migrations/20260805092201_Add_Audit_Columns_To_Orders.Designer.cs
+++ src/Modules/Orders/Orders.Infrastructure/Migrations/20260805092201_Add_Audit_Columns_To_Orders.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Orders.Infrastructure.Database;
@@ -11,9 +12,11 @@ using Orders.Infrastructure.Database;
 namespace Orders.Infrastructure.Migrations
 {
     [DbContext(typeof(OrdersDbContext))]
-    partial class OrdersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260805092201_Add_Audit_Columns_To_Orders")]
+    partial class Add_Audit_Columns_To_Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Orders/Orders.Infrastructure/Migrations/20260805092201_Add_Audit_Columns_To_Orders.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Audit_Columns_To_Orders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                schema: "orders",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                schema: "orders",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                schema: "orders",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "orders",
+                table: "Orders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedAt",
+                schema: "orders",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                schema: "orders",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                schema: "orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedAt",
+                schema: "orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                schema: "orders",
+                table: "Orders");
+        }
+    }
+}
