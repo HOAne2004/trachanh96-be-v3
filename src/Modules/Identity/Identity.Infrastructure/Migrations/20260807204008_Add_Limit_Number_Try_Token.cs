2f5ba0a46152602cb127e81528dc66f2abed3482
HOAn04 <lehuyhoan04@gmail.com>
2026-08-08 13:01:00 +0700

    feat(identity): add limit try token in PasswordResetToken and VerificationToken

---
+++ src/Modules/Identity/Identity.Infrastructure/Migrations/20260807204008_Add_Limit_Number_Try_Token.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Limit_Number_Try_Token : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmailVerificationAttempts",
+                schema: "identity",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PasswordResetAttempts",
+                schema: "identity",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationAttempts",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetAttempts",
+                schema: "identity",
+                table: "Users");
+        }
+    }
+}
