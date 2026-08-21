7719f7e4ff74a8db178ef39622fc8d2d5ad0841f
HOAn04 <lehuyhoan04@gmail.com>
2026-08-21 08:12:40 +0700

    fix(identity): add OutboxMessage to fix it has not declared in IdentityDbContext

---
+++ src/Modules/Identity/Identity.Infrastructure/Database/Outbox/OutboxMessageConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shared.Infrastructure.Outbox;
+
+namespace Identity.Infrastructure.Database.Outbox;
+
+public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.ToTable("OutboxMessages", "identity");
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => new { x.ProcessedOnUtc, x.OccurredOnUtc });
+    }
+}
